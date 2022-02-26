06e64b903f19074337552e28cd7f0692c876fe33
Auricelio <auricelio.suporte@hotmail.com>
2022-02-26 20:55:52 -0300

    Front and Backend

---
+++ frontend/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css'
+
+const ButtonIcon = () => {
+    return (
+        <div>
+            <button className="btn btn-primary btn-icon">
+                <h6>INICIE AGORA A SUA BUSCA</h6>
+            </button>
+        </div>
+
+        
+    );
+}
+
+export default ButtonIcon;
---
+++ frontend/src/pages/Admin/index.tsx
@@ -0,0 +1,10 @@
+const Admin = () => {
+
+    return (
+        <div>
+            <h1>Página do Admin</h1>
+        </div>
+    );
+}
+
+export default Admin;
