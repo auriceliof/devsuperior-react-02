40c4202fa0460a4c068b9549573a06378cc7802a
Auricelio <auricelio.suporte@hotmail.com>
2022-03-05 17:59:44 -0300

    First request Axios

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,10 +1,22 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import axios from 'axios';
 import ProductPrice from 'components/ProductPrice';
 import { Link } from 'react-router-dom';
+import { Product } from 'types/product';
+import { BASE_URL } from 'utils/requests';
 
 import './styles.css';
 
 const ProductDetails = () => {
+
+  //FORMA INCORRETA
+  let product : Product;
+
+  axios.get(BASE_URL + "/products/2")
+    .then(response => {
+        console.log(response.data)
+    });
+
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
