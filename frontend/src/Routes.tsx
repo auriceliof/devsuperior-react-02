fe9b7f6e8dab2e7efdc096178a24baa6a1da1473
Auricelio <auricelio.suporte@hotmail.com>
2022-03-05 16:42:00 -0300

    Routes, redirectins, nesting

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -3,7 +3,7 @@ import Admin from "pages/Admin";
 import Catalog from "pages/Catalog";
 import ProductDetails from "pages/ProductDetails";
 import Home from "pages/Home";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 
 const Routes = () => (
@@ -22,6 +22,8 @@ const Routes = () => (
             <Route path="/products/:productId">
                 <ProductDetails />
             </Route>
+
+            <Redirect from="/admin" to="/admin/products" exact />
 
             <Route path="/admin">
                 <Admin />
